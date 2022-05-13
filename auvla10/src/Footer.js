0e4a2456bb5005d58b5ed4ef8c3c740588902564
Bárbara Novaes <101150181+babidln@users.noreply.github.com>
2022-05-12 21:18:03 -0300

    Add files via upload

---
+++ auvla10/src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <h1>Boa noite para todos!</h1>
+      <h2> Vamos iniciar no react</h2>
+      </div>
+  );
+}
+
+export default Footer;
---
+++ auvla10/src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>Boa noite para todos!</h1>
+      <h2> Vamos iniciar no react</h2>
+      </div>
+  );
+}
+
+export default Header;
